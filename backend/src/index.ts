7ed00c32b49eaaa03d0e217cecb05eea8beb6cdd
asishkumarv <asishkumarv@outlook.com>
2026-06-09 17:48:24 +0530

    cors acceptance

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,12 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:5174',
     'http://localhost:3000',
+    'http://localhost:8080',
+    'http://localhost:8081',
+    'http://localhost:8082',
+    'http://localhost:8083',
+    'http://localhost:8084',
+    'http://localhost:8085',
     'https://admin.kalasamskruthiarts.in',
     'https://kalasamskruthiarts.in',
     'https://www.kalasamskruthiarts.in'
